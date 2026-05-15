75e9fd5e5d52cbdf2db13591368f2f4c5524f444
gcshearman <gemma.shearman@gmail.com>
2026-05-15 12:22:28 +0100

    Test worksheet

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Physics Lab Worksheet</h1>
+
+      <label>
+        Voltage:
+        <input type="number" />
+      </label>
+    </div>
+  );
+}
+
+export default App
